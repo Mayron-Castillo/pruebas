bab2b035ebd0ae72bf49f3d786093eeefa364bd7
Mayron Castillo <12mayron12@gmail.com>
2025-02-24 21:23:12 -0600

    practicando async await y trabajando con APIs

--- script.js
+++ script.js
@@ -710,16 +710,21 @@
 
 // main();
 
+// async function weather() {
+//     try {
+//         const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
+//         if(!response.ok){
+//             throw new Error("error");
+//         }
+//         const data = await response.json();
+//         console.log(data.latitude);
+//         console.log(data.timezone);    
+//     } catch (error) {
+//         console.log(error);
+//     }
 
-
-
-
-
-
-
-
-
-
+// }
+// weather();
 
 
 
